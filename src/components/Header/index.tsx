0cd33fb691ffe7f60fa4d28e827bcca3090c955e
Mike Cooper <mythmon@gmail.com>
2019-01-20 22:29:53 -0800

    Put menu contianer back

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -14,24 +14,26 @@ export default function Header() {
       color="red"
       style={{ borderRadius: 0 }}
     >
-      <Menu.Item as={Link} to="/">
-        <Image src={logoUrl} spaced="right" className="logo" />
-        Battle Buddy
-      </Menu.Item>
-      <Menu.Item as={Link} to="/pokemon">
-        Pokemon
-      </Menu.Item>
-      <Menu.Item as={Link} to="/types">
-        Types
-      </Menu.Item>
-      <Menu.Item
-        icon
-        position="right"
-        as="a"
-        href="https://github.com/mythmon/battle-buddy"
-      >
-        <Icon name="github" />
-      </Menu.Item>
+      <Container>
+        <Menu.Item as={Link} to="/">
+          <Image src={logoUrl} spaced="right" className="logo" />
+          Battle Buddy
+        </Menu.Item>
+        <Menu.Item as={Link} to="/pokemon">
+          Pokemon
+        </Menu.Item>
+        <Menu.Item as={Link} to="/types">
+          Types
+        </Menu.Item>
+        <Menu.Item
+          icon
+          position="right"
+          as="a"
+          href="https://github.com/mythmon/battle-buddy"
+        >
+          <Icon name="github" />
+        </Menu.Item>
+      </Container>
     </Menu>
   );
 }
